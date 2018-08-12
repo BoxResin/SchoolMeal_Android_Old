2aa394be169204827ab68bfac67c77f15b845cf6
Minsuk Eom <boxresin@gmail.com>
2018-08-12 22:02:59 +0900

    학교 검색시 행정구역 정보도 받도록 함

--- app/src/main/java/winapi251/app/schoolmeal/model/school/FakeSchoolSearcher.kt
+++ app/src/main/java/winapi251/app/schoolmeal/model/school/FakeSchoolSearcher.kt
@@ -5,7 +5,7 @@ import winapi251.app.schoolmeal.model.Area
 /** 가짜 학교 검색기 */
 object FakeSchoolSearcher : SchoolSearcher
 {
-    override fun search(schoolName: String): List<SchoolInfo>
+    override fun search(area: Area, schoolName: String): List<SchoolInfo>
     {
         val sample = listOf(
                 SchoolInfo(code = "B100000925", courseCode = "2", kindCode = "02",
@@ -20,6 +20,6 @@ object FakeSchoolSearcher : SchoolSearcher
                         name = "선린인터넷고등학교", address = "서울특별시 용산구 청파동3가",
                         area = Area.SEO_UL)
         )
-        return sample.filter { schoolName in it.name }
+        return sample.filter { area == it.area && schoolName in it.name }
     }
 }
--- app/src/main/java/winapi251/app/schoolmeal/model/school/SchoolSearcher.kt
+++ app/src/main/java/winapi251/app/schoolmeal/model/school/SchoolSearcher.kt
@@ -1,14 +1,17 @@
 package winapi251.app.schoolmeal.model.school
 
+import winapi251.app.schoolmeal.model.Area
+
 /** 학교 검색기 */
 interface SchoolSearcher
 {
     /**
      * 학교를 검색한다.
+     * @param area       행정구역
      * @param schoolName 학교명
      * @return 검색된 학교 목록
      */
-    fun search(schoolName: String): List<SchoolInfo>
+    fun search(area: Area, schoolName: String): List<SchoolInfo>
 
     // 디폴트 동작 설정
     companion object : SchoolSearcher by FakeSchoolSearcher
